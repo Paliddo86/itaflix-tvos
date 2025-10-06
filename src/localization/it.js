f149d1a6f504b40f92da6811ae49fdcca196f9ca
Paliddo86 <L.pallante86@gmail.com>
2025-10-07 00:34:40 +0200

    Aggiunta nuove chiavi di localizzazione per le stagioni e gli episodi delle serie TV

--- src/localization/it.js
+++ src/localization/it.js
@@ -107,10 +107,11 @@ export default {
   'tvshow-episode-title': ({ title_en }) => title_en || '',
   'tvshow-episode-airdate': ({ airdate }) => `Andato in onda ${airdate}`,
   'tvshow-episode-menu-hint': 'tieni premuto per aprire il menù episodio',
-  'tvshow-season': ({ seasonNumber }) => `Stagione ${seasonNumber}`,
+  'tvshow-season': (seasonNumber) => `Stagione ${seasonNumber}`,
   'tvshow-next': 'Nuovi Episodi',
   'tv-show-updates': 'Aggiornamenti Serie TV',
   'tvshow-new': 'Nuovo',
+  'tvshow-season-episode': ({ seasonNumber, episodeNumber }) => `St ${seasonNumber} - Ep ${episodeNumber}`,
 
   'movie-updates': 'Aggiornamenti Film',
   'movie-status': 'Stato',
@@ -122,8 +123,11 @@ export default {
   'movie-cast-crew': 'Attori e Cast',
   'movie-quality': 'Qualità',
   'movie-collection': 'Collezione',
-
-  'tvshow-status': 'Stato',
+  'movie-type': 'Film',
+  
+  'tvshow-type': 'Serie TV',
+  'season-type': 'Stagione',
+  'tvshow-status': 'Stato Serie TV',
   'tvshow-genres': 'Generi',
   'tvshow-actors': 'Attori',
   'tvshow-seasons': 'Stagioni',
@@ -166,6 +170,7 @@ export default {
   'status-in production': 'In Produzione',
   'status-released': 'Rilasciato',
   'status-post production': 'Post Produzione',
+  'status-returning series': 'Serie Rinnovata',
 
   'tvshow-control-continue-watching': 'Continua a guardare',
   'tvshow-control-show-trailer': 'Play\nTrailer',
@@ -271,4 +276,5 @@ export default {
   'error': 'Errore',
   'generic-error': 'Errore generico',
   'loading': 'Caricamento...'
+  
 };
