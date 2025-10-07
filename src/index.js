b34f590a39c678f8963590c71443ff57dc7509cc
Paliddo86 <L.pallante86@gmail.com>
2025-10-07 21:01:25 +0200

    Rimosso codice non utilizzato e semplificata la gestione delle rotte nel file index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import * as TVDML from 'tvdml';
 import * as user from './user';
 
 import { get as i18n } from './localization';
-import { checkSession, getUiData } from './request/adc';
-import { getOpenURLParams } from './utils';
 
 import menuRoute from './routes/menu';
 import myRoute from './routes/my';
@@ -19,22 +17,7 @@ import settingsRoute from './routes/settings';
 import movieRoute from './routes/movie';
 import tvShowsGenresRoute from './routes/tvShowsGenres';
 import homeRoute from './routes/home';
-
-import { AUTH, BASIC, GUEST } from './routes/menu/constants';
-
 import Loader from './components/loader';
-
-function openURLHandler(openURL) {
-  const mainRoute = navigationDocument.documents.find(
-    ({ route }) => route === 'main',
-  );
-
-  if (mainRoute) {
-    navigationDocument.popToDocument(mainRoute);
-  }
-
-  TVDML.navigate(...getOpenURLParams(openURL));
-}
 
 TVDML.subscribe(TVDML.event.LAUNCH).pipe(params => {
   /**
@@ -47,8 +30,6 @@ TVDML.subscribe(TVDML.event.LAUNCH).pipe(params => {
 
 TVDML.handleRoute('get-token')
   .pipe(TVDML.render(<Loader title={i18n('auth-checking')} />))
-  // .pipe(getUiData)
-  // .pipe(checkSession)
   .pipe(payload => {
     if(process.env.NODE_ENV === "development") console.log("Paylod User", payload); 
     user.set({ logged: 0});
@@ -62,13 +43,9 @@ TVDML.handleRoute('main').pipe(
   menuRoute([
     {
       route: 'home',
-      //active: AUTH,
-      active: GUEST,
     },
     {
       route: 'search',
-      //active: AUTH,
-      active: GUEST,
     },
     {
       route: 'moviesGenres',
