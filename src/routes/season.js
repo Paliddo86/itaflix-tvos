6fe18177c684d441c030828d88d0ff50c51332a1
Paliddo86 <L.pallante86@gmail.com>
2025-10-23 21:08:16 +0200

    Rimosso il termine "altadefinizione" dai keywords e migliorata la gestione dei media nella funzione di preparazione del player

--- src/routes/season.js
+++ src/routes/season.js
@@ -443,16 +443,16 @@ export default function seasonRoute() {
             //const episode = getEpisode(episodeNumber, episodes);
 
             const preparePlayer = (link) => {
-                VixSrcService.createMediaItemWithSubtitles(link).then(mediaItem => {
-                  mediaItem.title = `${tvshow.title} - ${i18n('tvshow-season-episode', { seasonNumber: season.number, episodeNumber: episode.number })}`
-                  mediaItem.description = episode.overview|| "";
-                  mediaItem.artworkImageURL =episode.poster || tvshow.cover;
-                  mediaItem.resumeTime = 0;
-                  
-                  player.playlist.push(movieMediaItem);
-                  player.play();
-                });
-                
+              let media = {
+                title: `${tvshow.title} - ${i18n('tvshow-season-episode', { seasonNumber: season.number, episodeNumber: episode.number })}`,
+                description: episode.overview|| "",
+                stream: link,
+                artworkImageURL: episode.poster || tvshow.cover,
+                resumeTime: 0
+              }
+                let tvshowMediaItem =  createMediaItems(media);
+                player.playlist.push(tvshowMediaItem);
+                player.play();
             }
 
             VixSrcService.getTvShowUrl(tvshow.tmdb_id, season.number, episode.number).then(link => {
